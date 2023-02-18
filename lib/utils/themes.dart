67b3126555249fb738888b0932c0944faaab6d63
Chooeychloe <dhan.belgica0427@gmail.com>
2023-02-18 18:45:53 +0800

    first commit

---
+++ lib/utils/themes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+CustomTheme currenTheme = CustomTheme();
+
+class CustomTheme with ChangeNotifier {
+  static bool _isDarkTheme = true;
+  ThemeMode get currentTheme => _isDarkTheme ? ThemeMode.dark : ThemeMode.light;
+
+  void toggleTheme() {
+    _isDarkTheme = !_isDarkTheme;
+    notifyListeners();
+  }
+}
